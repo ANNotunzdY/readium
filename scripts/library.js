d276b4fe7460913bc68b14ccf48092f2fcd13be5
Matthew Robertson <matthewr@evidentpoint.com>
2012-02-05 02:36:34 -0800

    refactor of library to Backbone.js

--- scripts/library.js
+++ scripts/library.js
@@ -1,55 +1,109 @@
-(function() {
+(function($) {
 	
-	var _lawnchair;
+	window.LibraryItem = Backbone.Model.extend({
+		// business logic goes hmyar
+		getViewBookUrl: function(book) {
+			return "/views/viewer.html?book=" + this.get('key');
+		},
 	
-	var getViewBookUrl = function(book) {
-		return "/views/viewer.html?book=" + book.key;
-	};
-	
-	var getDeleteLink = function(book) {
-		var confMessage = "Are you sure you want to perminantly delete "
-		return "<a class='delete-link btn danger' href='#' data-key='"+book.key+"' data-confirm='"+ confMessage + book.title +"'>delete</a>";
-	};
-	
-	var getBookHtml = function(book) {
-		var html;
-		html  = "<div class='book-item clearfix' id='" + book.key + "'>";
-		html += "<div class='info-wrap clearfix'><div class='caption book-info'>";
-		html += "<h3 class='green'>"+ book.title +"</h3>";
-		html += "<h6>"+ book.author +"</h6>";
-		html += "</div>";
-		html += "<img class='cover-image' src='" + book.cover_href + "' width='150' height='220' >";
-		html += "</div>";
-		html += '<div class="caption clearfix buttons">';
-		html +=	'<a href="' + getViewBookUrl(book) + '" class="btn read">Read</a>'; 
-		html +=	'<a href="' + getDeleteLink(book) + '" class="btn details">Details</a>';
-		html += "</div>";
-		html += "</div>";
-		return html;
-	};
-	
-	var removeFiles = function(book, callback) {
+		/*getDeleteLink:  function(book) {
+			var confMessage = "Are you sure you want to perminantly delete "
+			return "<a class='delete-link btn danger' href='#' data-key='"+book.key+"' data-confirm='"+ confMessage + book.title +"'>delete</a>";
+		}
+
+		var removeFiles = function(book, callback) {
 		Readium.FileSystemApi(function(fs) {
 			fs.rmdir(book.key);
 			callback();
 		});
 	};
 	
-	var deleteBook = function(key) {
-		Lawnchair(function() {
-			var that = this;
-			this.get(key, function(book) {
-				if(book) {
-					removeFiles(book, function() {	
-						that.remove(key);				
-						$("#" + key).toggle('fast');
-					});
-				}
-			});		
-		})
+		var deleteBook = function(key) {
+			Lawnchair(function() {
+				var that = this;
+				this.get(key, function(book) {
+					if(book) {
+						removeFiles(book, function() {	
+							that.remove(key);				
+							$("#" + key).toggle('fast');
+						});
+					}
+				});		
+			})
 
-	};
+		};*/
+	});
 
+	window.LibraryItems = Backbone.Collection.extend({
+
+		model: LibraryItem
+		
+	});
+
+	window.LibraryItemView = Backbone.View.extend({
+
+		tagName: 'div',
+
+		/*id: function() {
+			this.model.get('key');
+		},*/
+
+		className: "book-item clearfix",
+
+		initialize: function() {
+			_.bindAll(this, "render");
+			this.template = _.template( $('#library-item-template').html() );	
+		},
+
+		render: function() {
+			var renderedContent = this.template(this.model.toJSON());
+			$(this.el).html(renderedContent);
+			return this;
+		}
+	});
+
+	window.LibraryItemsView = Backbone.View.extend({
+		tagName: 'div',
+
+		id: "library-items-container",
+
+		className: 'row-view clearfix',
+
+		initialize: function() {
+			_.bindAll(this, "render");
+			this.template = _.template( $('#library-items-template').html() );
+			this.collection.bind('reset', this.render)
+		},
+
+		render: function() {
+			var collection = this.collection;
+			var $container = $(this.el);
+			$container.html(this.template({}));
+
+			// it the collection is empty break out early
+			if(collection.length === 0 ) {
+				$container.append("<p>Your book list is empty</p>");
+				return this;
+			}
+
+			collection.each(function(item) {
+				var view = new LibraryItemView({
+					model: item,
+					collection: collection,
+					id: item.get('id')
+				});
+				$container.append( view.render().el );
+
+			});
+			
+			// i dunno if this should go here
+			$('#library-books-list').html(this.el)
+			//setupClickHandlers();
+			return this;
+		}
+	});
+
+	
 	var addLibraryBooks =  function(records) {
 		var html;
 		if(records.length === 0) {
@@ -77,44 +131,52 @@
 		$('#loading-message').remove();
 	};
 		
-	var handleFileSelect = function(evt) {
-		var files = evt.target.files; // FileList object
-		var url = window.webkitURL.createObjectURL(files[0]);
-		
-	    // Create a new window to the info page.
-	    chrome.windows.create({ url: ('/views/extractBook.html#' + url), width: 1200, height: 760 });
-	};
 	
-	var clickHandler = function(evt) {
-		var input = document.getElementById('book-url');
-		if(input.value === null || input.value.length < 1) {
-			alert("invalid url, cannot process");
-		}
-		else {
-			var url = input.value;
-			chrome.windows.create({ url: ('/views/extractBook.html#' + url), width: 1200, height: 760 });
-		}
-	};
+
+	window.Library = new LibraryItems();
+
+
 
 	
 
-	$(function() {
-		document.getElementById('files').addEventListener('change', handleFileSelect, false);
-		document.getElementById('url-button').addEventListener('click', clickHandler, false);
-		_lawnchair = new Lawnchair(function() {
-			this.all(function(all) {
-				addLibraryBooks(all);				
-			});
+})(jQuery);
+
+var handleFileSelect = function(evt) {
+	var files = evt.target.files; // FileList object
+	var url = window.webkitURL.createObjectURL(files[0]);
+	
+    // Create a new window to the info page.
+    chrome.windows.create({ url: ('/views/extractBook.html#' + url), width: 1200, height: 760 });
+};
+
+var clickHandler = function(evt) {
+	var input = document.getElementById('book-url');
+	if(input.value === null || input.value.length < 1) {
+		alert("invalid url, cannot process");
+	}
+	else {
+		var url = input.value;
+		chrome.windows.create({ url: ('/views/extractBook.html#' + url), width: 1200, height: 760 });
+	}
+};
+
+$(function() {
+	document.getElementById('files').addEventListener('change', handleFileSelect, false);
+	document.getElementById('url-button').addEventListener('click', clickHandler, false);
+	_lawnchair = new Lawnchair(function() {
+		this.all(function(all) {
+			window.Library.reset(all);	
+			var lib = new LibraryItemsView({collection: window.Library});			
+			lib.render();
 		});
-		$("#block-view-btn").click(function(e) {
-			$('#library-items-container').addClass("block-view").removeClass("row-view")
-		});
-		$("#row-view-btn").click(function(e) {
-			$('#library-items-container').addClass("row-view").removeClass("block-view")
-		})
-		
 	});
-
-})();
+	$("#block-view-btn").click(function(e) {
+		$('#library-items-container').addClass("block-view").removeClass("row-view")
+	});
+	$("#row-view-btn").click(function(e) {
+		$('#library-items-container').addClass("row-view").removeClass("block-view")
+	})
+	
+});
 
 
